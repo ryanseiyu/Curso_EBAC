02dbc1ef4742d9339e1bd145c1645a80651da6a9
Ryan Seiyu <ryan_seiyu@hotmail.com>
2023-06-02 18:39:44 -0300

    satus

---
+++ src/typescript.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(a, b) {
+    return (a * b);
+}
+function saudacao(nome) {
+    return ("Olá " + nome);
+}
+console.log(multiplicar(2, 2));
+console.log(saudacao("Ryan"));
---
+++ src/typescript.ts
@@ -0,0 +1,13 @@
+
+function multiplicar(a: number, b: number): number {
+    return (a * b);
+}
+
+
+function saudacao(nome: string): string {
+    return ("Olá " + nome);
+}
+
+
+console.log(multiplicar(2,2))
+console.log(saudacao("Ryan"))
